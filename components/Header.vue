4225c8189bd5c03fb4a7d5a33fa2952cbb39730d
jon <kawadu1221@gmail.com>
2019-11-21 14:33:41 +0900

    Add header

---
+++ components/Header.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <nav
+      class="navbar hero is-primary"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div class="navbar-brand">
+        <a class="navbar-item">
+          Pianokun
+        </a>
+      </div>
+    </nav>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+</style>
