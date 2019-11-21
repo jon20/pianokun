bf7dbd6bfafbebdd20251b7364ba546316874468
jon <kawadu1221@gmail.com>
2019-11-21 14:33:35 +0900

    Add footer

---
+++ components/Footer.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <footer class="footer is-primary">
+      <div class="content has-text-centered">
+        © 2019 Jon
+      </div>
+    </footer>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.footer {
+  background: rgb(121, 121, 121);
+}
+</style>
